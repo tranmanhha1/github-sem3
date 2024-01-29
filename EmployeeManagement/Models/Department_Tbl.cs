dfbc277745958e02b8e3af33ea9401120565061a
tranmanhha1 <111476167+tranmanhha1@users.noreply.github.com>
2024-01-29 16:43:20 +0700

    f
    f

---
+++ EmployeeManagement/Models/Department_Tbl.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Location { get; set; }
+        public int NumberOfPersonals { get; set; }
+        public ICollection<Employee_Tbl> Employee { get; set; }
+        public int NumberOfEmployees { get; internal set; }
+    }
+
+}
---
+++ EmployeeManagement/Models/Employee_Tbl.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Employee_Tbl
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int DepartmentId { get; set; }
+        public string Rank { get; set; }
+
+        
+    }
+}
