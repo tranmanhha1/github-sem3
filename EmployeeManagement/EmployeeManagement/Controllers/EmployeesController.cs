249e23472d2feced85abc6af9afc452efcb1b9b1
tranmanhha1 <111476167+tranmanhha1@users.noreply.github.com>
2024-01-29 16:27:34 +0700

    vvv
    vvv

---
+++ EmployeeManagement/EmployeeManagement/Controllers/EmployeesController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using asp.net_exam.Data;
+using asp.net_exam.Models;
+
+namespace asp.net_exam.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly aspnet_examContext _context;
+
+        public EmployeesController(aspnet_examContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index(int? departmentId)
+        {
+            if (departmentId == null)
+            {
+                // Nếu departmentId không được truyền, trả về tất cả nhân viên
+                var allEmployees = await _context.Employee.Include(e => e.Department).ToListAsync();
+                return View(allEmployees);
+            }
+
+            // Nếu departmentId được truyền, chỉ trả về nhân viên thuộc phòng ban đó
+            var employeesInDepartment = await _context.Employee
+                .Where(e => e.DepartmentId == departmentId)
+                .Include(e => e.Department)
+                .ToListAsync();
+
+            return View(employeesInDepartment);
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Employee == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employee
+                .Include(e => e.Department)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            ViewData["DepartmentId"] = new SelectList(_context.Set<Department>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Code,DepartmentId,Rank")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(employee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Set<Department>(), "Id", "Id", employee.DepartmentId);
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Employee == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employee.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Set<Department>(), "Id", "Id", employee.DepartmentId);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Code,DepartmentId,Rank")] Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeeExists(employee.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Set<Department>(), "Id", "Id", employee.DepartmentId);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Employee == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employee
+                .Include(e => e.Department)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Employee == null)
+            {
+                return Problem("Entity set 'aspnet_examContext.Employee'  is null.");
+            }
+            var employee = await _context.Employee.FindAsync(id);
+            if (employee != null)
+            {
+                _context.Employee.Remove(employee);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return (_context.Employee?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/Data/EmployeeManagementContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Data
+{
+    public class EmployeeManagementContext : DbContext
+    {
+        public EmployeeManagementContext (DbContextOptions<EmployeeManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeManagement.Models.Employee_Tbl> Employee { get; set; } = default!;
+        public DbSet<EmployeeManagement.Models.Department> Department { get; set; } = default!;
+    }
+}
